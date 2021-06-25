52addf1728fba160f17ca080fefbcf46a7bafa30
jojobrogess <joeyburgess19@gmail.com>
2021-06-24 23:17:31 -0700

    Update libreelec.sh

--- libreelec.sh
+++ libreelec.sh
@@ -25,8 +25,7 @@ PIINFO=$(cat /flash/config.txt | grep 'dtparam=i2c=on')
 if [ -z "$PIINFO" ]
 then
 	mount -o remount,rw /flash
-	echo "dtparam=i2c=on" >> /flash/config.txt
-	echo "enable_uart=1" >> /flash/config.txt
+	echo "otg_mode=1" >> /flash/config.txt
 	mount -o remount,ro /flash
 fi
 	
