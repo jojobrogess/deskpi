383ac85d7328dbc336c14cfb3d191d30f21c503b
jojobrogess <joeyburgess19@gmail.com>
2021-06-30 03:02:55 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # 
 . /lib/lsb/init-functions
-cd ~
-sh -c "git clone https://github.com/DeskPi-Team/deskpi.git"
 cd $HOME/deskpi/
 daemonname="deskpi"
 tempmonscript=/usr/bin/pmwFanControl
@@ -18,12 +16,12 @@ if [ -e $deskpidaemon ]; then
 	sudo rm -f $deskpidaemon
 fi
 
-# adding dtoverlay to enable dwc2 on host mode.
-log_action_msg "Enable dwc2 on Host Mode"
-sudo sed -i '/dtoverlay=dwc2*/d' /boot/config.txt 
-sudo sed -i '$a\dtoverlay=dwc2,dr_mode=host' /boot/config.txt 
-if [ $? -eq 0 ]; then
-   log_action_msg "dwc2 has been setting up successfully"
+PIINFO=$(cat /flash/config.txt | grep 'otg_mode=1')
+if [ -z "$PIINFO" ]
+then
+	mount -o remount,rw /flash
+	echo "otg_mode=1" >> /flash/config.txt
+	mount -o remount,ro /flash
 fi
 # install PWM fan control daemon.
 log_action_msg "DeskPi main control service loaded."
