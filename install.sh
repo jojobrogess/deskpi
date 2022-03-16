6924748cad0da13cc829b5dc04aff619d8dd7d96
jojobrogess <joeyburgess19@gmail.com>
2022-03-15 17:20:20 -0700

    keep on keeping on

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ####init library should be installed first prior to anything####
 daemonname="deskpi"
-installationfolder=$HOME/$daemonname
+installationfolder=$HOME/$daemonname-Test
 
 deskpi_create_file() {
 	if [ -f $1 ]; then
@@ -11,37 +11,26 @@ deskpi_create_file() {
 	chmod 666 $1
 }
 
-#if [ -e "/storage/user/lib/lsb" ] ; then
-#	rm -r /storage/user/lib/lsb
-#fi
-#
-#if [ ! -d "/storage/user" ] ; then
-#	mkdir -p /storage/user
-#	mkdir -p /storage/user/lib
-#	mkdir -p /storage/user/bin
-#fi
-#
+if [ -e "/storage/user/lib/lsb" ] ; then
+	rm -r /storage/user/lib/lsb
+fi
+
+if [ ! -d "/storage/user" ] ; then
+	mkdir -p /storage/user
+	mkdir -p /storage/user/lib
+	mkdir -p /storage/user/bin
+fi
+
 # Install init library.
-#cp -rf $installationfolder/lib/lsb /storage/user/lib/
-#chmod +x $installationfolder/lib/lsb/init-functions
-#
+cp -rf $installationfolder/lib/lsb /storage/user/lib/
+chmod +x $installationfolder/lib/lsb/init-functions
+
 ################################################################
 ####Set functions and values####
 . /storage/user/lib/lsb/init-functions
 daemonname="deskpi"
-powerbuttonscript=/storage/.config/$daemonname.py
 shutdaemonscript=/storage/.config/$daemonname-safeshut.service
-tempmonscript=/storage/user/bin/pmwFanControl.py
-shutdaemonscript=/storage/.config/$daemonname-safeshut.service
-deskpidaemon=/storage/.config/system.d/$daemonname.service
-
-# daemonname="deskpi"
-# powerbuttonscript=/storage/.config/$daemonname.py
-# shutdownscript="/storage/.config/"$daemonname"-poweroff.py"
-# daemonconfigfile=/storage/$daemonname.conf
-# configscript=/storage/deskpi-config
-# removescript=/storage/deskpi-uninstall
-# daemonfanservice=/storage/.config/system.d/$daemonname.service
+deskpidaemon=/storage/.config/$daemonname.service
 
 ####Check for Previous install####
 # install wiringPi library.
@@ -54,30 +43,31 @@ if [ -e $deskpidaemon ]; then
 fi
 
 # Create safe shut off service file on system.
-if [ -e $deskpidaemon-safeshut ]; then
-	rm -f $deskpidaemon
+if [ -e $shutidaemonscript ]; then
+	rm -f $shutdaemonscript
 	touch /storage/.config/system.d/$daemonname-safeshut.service
 fi
 
 ####Start Deskpi Install####
 # Check and enable otg_mode
-PIINFO=$(cat /flash/config.txt | grep 'CONFIG_USB_DWC2=y,dtoverlay=dwc2,dr_mode=host')
+PIINFO=$(cat /flash/config.txt | grep 'otg_mode=1,dtoverlay=dwc2,dr_mode=host')
 if [ -z "$PIINFO" ]
 then
 	mount -o remount,rw /flash
-	echo "CONFIG_USB_DWC2=y,dtoverlay=dwc2,dr_mode=host" >> /flash/config.txt
+	echo "otg_mode=1,dtoverlay=dwc2,dr_mode=host" >> /flash/config.txt
 	mount -o remount,ro /flash
 fi
 
 #Install config files.
 cp -rf $installationfolder/deskpi-config /storage/user/bin/
-cp -rf $installationfolder/Deskpi-uninstall /storage/user/bin/
 chmod 755 /storage/user/bin/deskpi-config
-chmod 755 /storage/user/bin/Deskpi-uninstall
 
-# Install "C" PWM fan control daemon.
+#Installsafecutoffpower
+cp -rf $installationfolder/drivers/safecutoffpower /storage/user/bin
+chmod 755 /storage/user/bin/safecutoffpower
+
+# Install PWM fan control daemon.
 log_action_msg "DeskPi main control service loaded."
-cd $installationfolder/drivers/c/ 
 cp -rf $installationfolder/drivers/pwmFanControl /storage/user/bin/
 cp -rf $installationfolder/drivers/fanStop  /storage/user/bin/
 chmod 755 /storage/user/bin/pwmFanControl
@@ -95,18 +85,18 @@ echo "[Install]" >> $deskpidaemon
 echo "WantedBy=multi-user.target" >> $deskpidaemon
 
 # send signal to MCU before system shuting down.
-echo "[Unit]" > $safeshutdaemon
-echo "Description=DeskPi Safeshutdown Service" >> $safeshutdaemon
-echo "Conflicts=reboot.target" >> $safeshutdaemon
-echo "Before=halt.target shutdown.target poweroff.target" >> $safeshutdaemon
-echo "DefaultDependencies=no" >> $safeshutdaemon
-echo "[Service]" >> $safeshutdaemon
-echo "Type=oneshot" >> $safeshutdaemon
-echo "ExecStart=/storage/user/bin/fanStop" >> $safeshutdaemon
-echo "RemainAfterExit=yes" >> $safeshutdaemon
-echo "TimeoutSec=1" >> $safeshutdaemon
-echo "[Install]" >> $safeshutdaemon
-echo "WantedBy=halt.target shutdown.target poweroff.target" >> $safeshutdaemon
+echo "[Unit]" > $shutdaemonscript
+echo "Description=DeskPi Safeshutdown Service" >> $shutdaemonscript
+echo "Conflicts=reboot.target" >> $shutdaemonscript
+echo "Before=halt.target shutdown.target poweroff.target" >> $shutdaemonscript
+echo "DefaultDependencies=no" >> $shutdaemonscript
+echo "[Service]" >> $shutdaemonscript
+echo "Type=oneshot" >> $shutdaemonscript
+echo "ExecStart=/storage/user/bin/fanStop" >> $shutdaemonscript
+echo "RemainAfterExit=yes" >> $shutdaemonscript
+echo "TimeoutSec=1" >> $shutdaemonscript
+echo "[Install]" >> $shutdaemonscript
+echo "WantedBy=halt.target shutdown.target poweroff.target" >> $shutdaemonscript
 
 log_action_msg "DeskPi Service configuration finished." 
 
@@ -116,6 +106,7 @@ systemctl daemon-reload
 systemctl enable $daemonname.service
 systemctl start $daemonname.service &
 systemctl enable $daemonname-safeshut.service
+systemctl start $daemonname-safeshut.service
 
 # Finished 
 log_success_msg "DeskPi PWM Fan Control and Safeshut Service installed successfully." 
--- uninstall.sh
+++ uninstall.sh
@@ -3,32 +3,33 @@
 . /storage/user/lib/lsb/init-functions
 
 daemonname='deskpi'
-deskpidaemon='/storage/.config/system.d/$daemonname.service'
-safeshutdaemon='/storage/.config/system.d/$daemonname-safeshut.service'
+deskpidaemon='/storage/.config/$daemonname.service'
+safeshutdaemon='/storage/.config/$daemonname-safeshut.service'
 
 log_action_msg "Uninstalling DeskPi PWM Fan Control and Safeshut Service."
 sleep 1
-log_action_msg "Diable DeskPi PWM Fan Control and Safeshut Service."
 log_action_msg "Remove otg_mode=1 configure from /flash/config.txt file"
 
 PIINFO=$(cat /flash/config.txt | grep 'otg_mode=1')
 if [ -n "$PIINFO" ]
 then
 	mount -o remount,rw /flash
-	    sed -i '$d' /flash/config.txt # Probably not a good idea to just delete the last line rather than find and delete.
+	    sed -i 'otg_mode=1,dtoverlay=dwc2,dr_mode=host' /flash/config.txt # Probably not a good idea to just delete the last line rather than find and delete.
 	mount -o remount,ro /flash
 fi
+log_action_msg "Removed otg_mode=1 configure from /flash/config.txt file"
 
-log_action_msg "Stop and disable DeskPi services"
+log_action_msg "Diable DeskPi PWM Fan Control and Safeshut Service."
+
 systemctl disable $daemonname.service 2&>/dev/null  
 systemctl stop $daemonname.service  2&>/dev/null
 systemctl disable $daemonname-safeshut.service 2&>/dev/null
 systemctl stop $daemonname-safeshut.service 2&>/dev/null
+
 log_action_msg "Remove DeskPi PWM Fan Control and Safeshut Service."
 rm -f  $deskpidaemon  2&>/dev/null 
 rm -f  $safeshutdaemon 2&>/dev/null 
 rm -f /storage/user/bin/fanStop 2&>/dev/null
 rm -f /storage/user/bin/pwmFanControl 2&>/dev/null 
 rm -f /storage/user/bin/deskpi-config 2&>/dev/null 
-rm -f /storage/user/bin/Deskpi-uninstall 2&>/dev/null 
 log_success_msg "Uninstall DeskPi Driver Successfully." 
